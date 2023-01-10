c58a443651330d6304b10b56e51d3d82b19bfc5a
Giselle026 <giselle.castro@a2odev.com>
2023-01-10 09:55:56 -0400

    feature/RI-710

--- tests/contactForm.spec.ts
+++ tests/contactForm.spec.ts
@@ -1,17 +1,17 @@
 describe('Contact Form', () => {
   it('should have a complete UI', async () => {
-    await browser.url('https://staging.a2odev.com/')
+    await browser.url('https://staging-new.a2odev.com/')
     
     await expect($('#name')).toBeExisting()
     await expect($('#email')).toBeExisting()
   })
   it('should send a success message', async () => {
-    await browser.url('https://staging.a2odev.com/')
+    await browser.url('https://staging-new.a2odev.com/')
 
-    await $('#name').setValue('user test')
+    await $('#name').setValue('Giselle')
     await $('#email').setValue('test@mail.com')
-    await $('[name="website"]').setValue('')
-    await $('[name="message"]').setValue('user test')
+    await $('[name="website"]').setValue('https://www.texasmotorspeedway.com/')
+    await $('[name="message"]').setValue('Service de la empresa')
     await $('button[type="submit"]').click()
 
     await expect($('.wpcf7-response-output')).toBeExisting()
