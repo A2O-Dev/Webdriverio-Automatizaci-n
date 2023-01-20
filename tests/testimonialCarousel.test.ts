fe1b460309b79e53fe0e885138ee466b922062cb
Karina Cabrera <kart.p14@gmail.com>
2023-01-20 12:29:48 -0400

    RI-708 Requested changes added-2

--- tests/testimonialCarousel.test.ts
+++ tests/testimonialCarousel.test.ts
@@ -14,10 +14,10 @@ describe('Testimonial - Carousel', () => {
 
         await testimonialActive.dragAndDrop({ x: 100, y: 0 })
         const coryDrag = await $('.swiper-slide-next .slick-slide').$('.elementskit-author-name=Cory Clemmons')
+        
         await browser.pause(2000)       
         await testimonialActive.dragAndDrop({ x: -100, y: 0 })
         await browser.pause(3000)
-
         await expect(coryDrag).toBeExisting()
         await expect(eric).toBeExisting()
 
@@ -25,10 +25,10 @@ describe('Testimonial - Carousel', () => {
 
         await $('.slick-next').click()
         const stephen = await $('.swiper-slide-next .slick-slide').$('.elementskit-author-name=Stephen Beehler')
+        
         await browser.pause(3000)
         await $('.slick-prev').click()
-        await browser.pause(3000)
-        
+        await browser.pause(3000)      
         await expect(stephen).toBeExisting()
         await expect(eric).toBeExisting()
     })
