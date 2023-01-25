44e0af713f0d8ee41cb3dc771c78410eb11ecca4
Karina Cabrera <kart.p14@gmail.com>
2023-01-25 19:25:36 -0400

    Adding a loop to the testimonial slider.

--- tests/testimonialCarousel.spec.ts
+++ tests/testimonialCarousel.spec.ts
@@ -2,35 +2,61 @@ describe('Testimonial - Carousel', () => {
     it('Check the testimonial carousel', async() => {
         await browser.url('https://staging-new.a2odev.com/')
         await browser.pause(3000)
-        browser.maximizeWindow()
+        await browser.maximizeWindow()
         
-        const testimonialCardDrag = await $('.swiper-slide-active .slick-slide')
+        const testimonialCardLeft = await $('.swiper-slide-active .slick-slide')
         const testimonialEric = await $('.swiper-slide-next .slick-slide').$('.elementskit-author-name=Eric Goldman')
-
+        const testimonialCory = await $('.swiper-slide-active .slick-slide').$('.elementskit-author-name=Cory Clemmons')
+        
         await expect($('h1=Testimonials')).toBeExisting()
-        testimonialCardDrag.click()  
-        await browser.pause(3000)
+        testimonialCardLeft.click()  // centrar seccion
+        await browser.pause(2000)
 
         // Drag and drop sliding
 
-        testimonialCardDrag.dragAndDrop({ x: 100, y: 0 })
-        const testimonialCory = await $('.swiper-slide-next .slick-slide').$('.elementskit-author-name=Cory Clemmons')
-        
-        await browser.pause(2000)       
-        testimonialCardDrag.dragAndDrop({ x: -100, y: 0 })
-        await browser.pause(3000)
-        expect(testimonialCory).toBeExisting()
-        expect(testimonialEric).toBeExisting()
+            // Left -> Rigth
+        for (let index = 1; index <= 5; index++) {
+            if (index<=5) {
+                await $('.swiper-slide-active .slick-slide').dragAndDrop({ x: 100, y: 0 })
+                await browser.pause(1000)
+            }else{
+                expect (testimonialCory).toBeExisting()
+                expect (testimonialEric).toBeExisting()
+            }
+        }
+
+            // Rigth -> Left
+        for (let index = 1; index <= 5; index++) {
+            if (index<=5) {
+                await $('.swiper-slide-active .slick-slide').dragAndDrop({ x: -100, y: 0 })
+                await browser.pause(1000)
+            }else{
+                expect (testimonialCory).toBeExisting()
+                expect (testimonialEric).toBeExisting()
+            }  
+        }      
 
         // Sliding with buttons
 
-        await $('.slick-next').click()
-        const testimonialStephen = await $('.swiper-slide-next .slick-slide').$('.elementskit-author-name=Stephen Beehler')
-        
-        await browser.pause(3000)
-        await $('.slick-prev').click()
-        await browser.pause(3000)      
-        expect(testimonialStephen).toBeExisting()
-        expect(testimonialEric).toBeExisting()
+            // button Next
+        for (let index = 1; index <= 5; index++) {
+            if (index<=5) {
+                await $('.slick-next').click()
+                await browser.pause(1000)
+            }else{
+                expect (testimonialCory).toBeExisting()
+                expect (testimonialEric).toBeExisting()
+            }  
+        }        
+            // button Prev
+        for (let index = 1; index <= 5; index++) {
+            if (index<=5) {
+                await $('.slick-prev').click()
+                await browser.pause(1000)
+            }else{
+                expect (testimonialCory).toBeExisting()
+                expect (testimonialEric).toBeExisting()
+            }  
+        }        
     })
 })
