4f79aefc61c0e03d296e2251cdf59365c0d4223d
Karina Cabrera <kart.p14@gmail.com>
2023-01-27 17:05:58 -0400

    RI-708: Adding validation of the existence of testimonials in the loop

--- tests/testimonialCarousel.spec.ts
+++ tests/testimonialCarousel.spec.ts
@@ -1,13 +1,13 @@
 describe('Testimonial - Carousel', () => {
-    it('Check the testimonial carousel', async() => {
+    it('Check the testimonial carousel', async () => {
         await browser.url('https://staging-new.a2odev.com/')
         await browser.pause(3000)
         await browser.maximizeWindow()
-        
+
         const testimonialCardLeft = await $('.swiper-slide-active .slick-slide')
         const testimonialEric = await $('.swiper-slide-next .slick-slide').$('.elementskit-author-name=Eric Goldman')
         const testimonialCory = await $('.swiper-slide-active .slick-slide').$('.elementskit-author-name=Cory Clemmons')
-        
+
         await expect($('h1=Testimonials')).toBeExisting()
         testimonialCardLeft.click()  // centrar seccion
         await browser.pause(2000)
@@ -15,48 +15,130 @@ describe('Testimonial - Carousel', () => {
         // Drag and Drop sliding
 
             // Left -> Rigth
-        for (let index = 1; index <= 5; index++) {
-            if (index<=5) {
-                await $('.swiper-slide-active .slick-slide').dragAndDrop({ x: 100, y: 0 })
-                await browser.pause(1000)
-            }else{
-                expect (testimonialCory).toBeExisting()
-                expect (testimonialEric).toBeExisting()
+        for (let index = 5; index >= 1; index--) {
+            console.debug('INDEX : ' + index);
+            
+            switch (index) {
+                
+                case 1:
+                    await $('.swiper-slide-active .slick-slide').dragAndDrop({ x: 100, y: 0 })
+                    expect(testimonialCory).toBeExisting()
+                    expect(testimonialEric).toBeExisting()
+                    break; 
+                case 2:
+                    await $('.swiper-slide-active .slick-slide').dragAndDrop({ x: 100, y: 0 })
+                    expect(await $('.swiper-slide-active .slick-slide').$('.elementskit-author-name=Eric Goldman')).toBeExisting()
+                    break;
+                case 3:
+                    await $('.swiper-slide-active .slick-slide').dragAndDrop({ x: 100, y: 0 })
+                    expect(await $('.swiper-slide-active .slick-slide').$('.elementskit-author-name=Stephen Beehler')).toBeExisting()
+                    break;
+                case 4:
+                    await $('.swiper-slide-active .slick-slide').dragAndDrop({ x: 100, y: 0 })
+                    expect(await $('.swiper-slide-active .slick-slide').$('.elementskit-author-name=Arnaud Kuhlein')).toBeExisting()
+                    break;
+                case 5:
+                    await $('.swiper-slide-active .slick-slide').dragAndDrop({ x: 100, y: 0 })
+                    expect(await $('.swiper-slide-active .slick-slide').$('.elementskit-author-name=Jim Call')).toBeExisting()
+                    break;               
             }
+            await browser.pause(2000)
         }
 
             // Rigth -> Left
         for (let index = 1; index <= 5; index++) {
-            if (index<=5) {
-                await $('.swiper-slide-active .slick-slide').dragAndDrop({ x: -100, y: 0 })
-                await browser.pause(1000)
-            }else{
-                expect (testimonialCory).toBeExisting()
-                expect (testimonialEric).toBeExisting()
-            }  
-        }      
+            console.debug('index : ' + index);
+            
+            switch (index) {
+                
+                case 1:
+                    await $('.swiper-slide-active .slick-slide').dragAndDrop({ x: -100, y: 0 })
+                    expect(await $('.swiper-slide-next .slick-slide').$('.elementskit-author-name=Stephen Beehler')).toBeExisting()
+                    break;
+                case 2:
+                    await $('.swiper-slide-active .slick-slide').dragAndDrop({ x: -100, y: 0 })
+                    expect(await $('.swiper-slide-next .slick-slide').$('.elementskit-author-name=Arnaud Kuhlein')).toBeExisting()
+                    break;
+                case 3:
+                    await $('.swiper-slide-active .slick-slide').dragAndDrop({ x: -100, y: 0 })
+                    expect(await $('.swiper-slide-next .slick-slide').$('.elementskit-author-name=Jim Call')).toBeExisting()
+                    break;
+                case 4:
+                    await $('.swiper-slide-active .slick-slide').dragAndDrop({ x: -100, y: 0 })
+                    expect(await $('.swiper-slide-next .slick-slide').$('.elementskit-author-name=Cory Clemmons')).toBeExisting()
+                    break;
+                case 5:
+                    await $('.swiper-slide-active .slick-slide').dragAndDrop({ x: -100, y: 0 })
+                    expect(testimonialCory).toBeExisting()
+                    expect(testimonialEric).toBeExisting()
+                    break;                
+            }
+            await browser.pause(2000)
+        }
 
         // Sliding with buttons
 
             // button Next
         for (let index = 1; index <= 5; index++) {
-            if (index<=5) {
-                await $('.slick-next').click()
-                await browser.pause(1000)
-            }else{
-                expect (testimonialCory).toBeExisting()
-                expect (testimonialEric).toBeExisting()
-            }  
-        }        
-            // button Prev
-        for (let index = 1; index <= 5; index++) {
-            if (index<=5) {
-                await $('.slick-prev').click()
-                await browser.pause(1000)
-            }else{
-                expect (testimonialCory).toBeExisting()
-                expect (testimonialEric).toBeExisting()
-            }  
-        }        
+            console.debug('index : ' + index);         
+            switch (index) {           
+                case 1:
+                    await $('.slick-next').click()
+                    expect(await $('.swiper-slide-next .slick-slide').$('.elementskit-author-name=Stephen Beehler')).toBeExisting()
+                    break;
+                case 2:
+                    await $('.slick-next').click()
+                    expect(await $('.swiper-slide-next .slick-slide').$('.elementskit-author-name=Arnaud Kuhlein')).toBeExisting()
+                    break;
+                case 3:
+                    await $('.slick-next').click()
+                    expect(await $('.swiper-slide-next .slick-slide').$('.elementskit-author-name=Jim Call')).toBeExisting()
+                    break;
+                case 4:
+                    await $('.slick-next').click()
+                    expect(await $('.swiper-slide-next .slick-slide').$('.elementskit-author-name=Cory Clemmons')).toBeExisting()
+                    break;
+                case 5:
+                    await $('.slick-next').click()
+                    expect(testimonialCory).toBeExisting()
+                    expect(testimonialEric).toBeExisting()
+                    break;                
+            }
+            await browser.pause(2000)
+        }
+
+            // button Prev      
+        for (let index = 5; index >= 1; index--) {
+            console.debug('INDEX : ' + index);
+            
+            switch (index) {
+                
+                case 1:
+                    await $('.slick-prev').click()
+                    expect(testimonialCory).toBeExisting()
+                    expect(testimonialEric).toBeExisting()
+                    break; 
+                case 2:
+                    await $('.slick-prev').click()
+                    expect(await $('.swiper-slide-active .slick-slide').$('.elementskit-author-name=Eric Goldman')).toBeExisting()
+                    break;
+                case 3:
+                    await $('.slick-prev').click()
+                    expect(await $('.swiper-slide-active .slick-slide').$('.elementskit-author-name=Stephen Beehler')).toBeExisting()
+                    break;
+                case 4:
+                    await $('.slick-prev').click()
+                    expect(await $('.swiper-slide-active .slick-slide').$('.elementskit-author-name=Arnaud Kuhlein')).toBeExisting()
+                    break;
+                case 5:
+                    await $('.slick-prev').click()
+                    expect(await $('.swiper-slide-active .slick-slide').$('.elementskit-author-name=Jim Call')).toBeExisting()
+                    break;               
+            }
+            await browser.pause(2000)
+        }
+        
     })
+
+
 })
