b6bdc85caaa0bff6a5543c105bca8fa641615efb
lino <lino.valverde@a2odev.com>
2023-02-01 11:37:38 -0400

    feature/RI-709

---
+++ tests/contactUsFormEmailWrong.spec.ts
@@ -0,0 +1,41 @@
+describe('Contact Form', () => {
+    it('check de format email', async () => {
+        await browser.url('https://staging-new.a2odev.com/')
+        await browser.maximizeWindow()
+          
+        await $('#name').setValue('Lino')
+        await $('#email').setValue('test.com@gmail')
+        await $('[name="website"]').setValue('https://www.texasmotorspeedway.com/')
+        await $('[name="message"]').setValue('Service de la empresa')
+        await $('button[type="submit"]').click()
+          
+        await expect($('.wpcf7-response-output')).toBeExisting()
+        await expect($('.wpcf7-response-output')).toHaveTextContaining('One or more fields have an error. Please check and try again.')
+      })
+    it('check de format email', async () => {
+        await browser.url('https://staging-new.a2odev.com/')
+        await browser.maximizeWindow()
+          
+        await $('#name').setValue('Lino')
+        await $('#email').setValue('test.gmail@com')
+        await $('[name="website"]').setValue('https://www.texasmotorspeedway.com/')
+        await $('[name="message"]').setValue('Service de la empresa')
+        await $('button[type="submit"]').click()
+          
+        await expect($('.wpcf7-response-output')).toBeExisting()
+        await expect($('.wpcf7-response-output')).toHaveTextContaining('One or more fields have an error. Please check and try again.')
+    })
+    it('check de format email', async () => {
+        await browser.url('https://staging-new.a2odev.com/')
+        await browser.maximizeWindow()
+          
+        await $('#name').setValue('Lino')
+        await $('#email').setValue('gmail.com@test')
+        await $('[name="website"]').setValue('https://www.texasmotorspeedway.com/')
+        await $('[name="message"]').setValue('Service de la empresa')
+        await $('button[type="submit"]').click()
+          
+        await expect($('.wpcf7-response-output')).toBeExisting()
+        await expect($('.wpcf7-response-output')).toHaveTextContaining('One or more fields have an error. Please check and try again.')
+    })  
+  })
