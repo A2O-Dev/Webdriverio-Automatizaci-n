ed5aefaec7fec911fc5ec6d67871254ea270e11b
lino <lino.valverde@a2odev.com>
2023-02-08 16:29:52 -0400

    feature/RI-712

--- tests/languageTranslation.spec.ts
+++ tests/languageTranslation.spec.ts
@@ -1,25 +1,25 @@
 describe('Language Traslation', () => {
-    it('Language Traskation Test', async () => {
-        await browser.url('https://staging-new.a2odev.com/')
+    it('Language Traslation Test', async () => {
+        const url = 'https://staging-new.a2odev.com/'
+        await browser.url(url)
         await browser.pause(3000)
         await browser.maximizeWindow()
 
         const languageLink =await $('//*[@id="premium-nav-menu-item-8907"]')
         const subMenuLink=await $('//*[@id="premium-nav-menu-item-8907"]/ul')
-        
         const spanishFlag = await $('img[title="Spanish"]')
 
         languageLink.moveTo({})
         await expect($(spanishFlag)).toBeExisting()
         subMenuLink.click()
         await browser.pause(3000)
-        await expect(browser).toHaveUrl('https://staging-new.a2odev.com/es/')
+        await expect(browser).toHaveUrl(url + 'es/')
         await browser.pause(3000)
 
         languageLink.moveTo({})
         subMenuLink.click()
         await browser.pause(3000)
-        await expect(browser).toHaveUrl('https://staging-new.a2odev.com/')
+        await expect(browser).toHaveUrl(url)
         await browser.pause(3000)
     })
 })
