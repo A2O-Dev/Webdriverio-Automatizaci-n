52d6573ceaeff0196c3c8eef493defa422eeb09e
lino <lino.valverde@a2odev.com>
2023-02-08 15:59:10 -0400

    feature/RI-712

---
+++ tests/languageTranslation.spec.ts
@@ -0,0 +1,25 @@
+describe('Language Traslation', () => {
+    it('Language Traskation Test', async () => {
+        await browser.url('https://staging-new.a2odev.com/')
+        await browser.pause(3000)
+        await browser.maximizeWindow()
+
+        const languageLink =await $('//*[@id="premium-nav-menu-item-8907"]')
+        const subMenuLink=await $('//*[@id="premium-nav-menu-item-8907"]/ul')
+        
+        const spanishFlag = await $('img[title="Spanish"]')
+
+        languageLink.moveTo({})
+        await expect($(spanishFlag)).toBeExisting()
+        subMenuLink.click()
+        await browser.pause(3000)
+        await expect(browser).toHaveUrl('https://staging-new.a2odev.com/es/')
+        await browser.pause(3000)
+
+        languageLink.moveTo({})
+        subMenuLink.click()
+        await browser.pause(3000)
+        await expect(browser).toHaveUrl('https://staging-new.a2odev.com/')
+        await browser.pause(3000)
+    })
+})
