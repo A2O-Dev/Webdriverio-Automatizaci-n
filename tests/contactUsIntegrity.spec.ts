67cc14177cc93df2cc0b6d1cff598c0d020bad9c
lino <lino.valverde@a2odev.com>
2023-02-15 17:18:19 -0400

    feature/RI-719

---
+++ tests/contactUsIntegrity.spec.ts
@@ -0,0 +1,35 @@
+describe('Contact Form Integrity ', () => {
+    it('should have a complete UI', async () => {
+      await browser.url('https://staging-new.a2odev.com/')
+      await browser.maximizeWindow()
+
+      const facebookLink = await $('a[href="https://es-la.facebook.com/a2odev/"]')
+      const twitterLink = await $('a[href="https://twitter.com/a2odev"]')
+      const linkedinLink = await $('a[href="https://bo.linkedin.com/company/a2odev"]')
+      const contactUsLink = await $('=Contact us')
+      const prevElementIframe = await $('.elementor-widget-container')
+      const onlyElementIframe = await $('iframe')
+      const elementIframe = $(prevElementIframe).$(onlyElementIframe)
+
+      contactUsLink.click()
+      await browser.pause(3000)
+
+      await expect(elementIframe).toBeExisting()
+      await expect(elementIframe).toHaveAttribute('src', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3798.1358151833942!2d-63.201536485340014!3d-17.83226708111117!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x93f1e99569937a45%3A0xa5edf408ba327226!2sA2O%20Dev!5e0!3m2!1ses!2sbo!4v1671550857839!5m2!1ses!2sbo')
+      await expect($('#name')).toBeExisting()
+      await expect($('#email')).toBeExisting()
+      await expect($('input[name="website"]')).toBeExisting()
+      await expect($('textarea[name="message"]')).toBeExisting()
+      await expect($('//*[@id="wpcf7-f3878-p1934-o1"]/form/div[5]/p/button')).toBeExisting()
+      await expect($('button[type="submit"]')).toBeExisting()
+         
+      facebookLink.moveTo({})
+      await browser.pause(2000)
+      twitterLink.moveTo({})
+      await browser.pause(2000)
+      linkedinLink.moveTo({})
+      await browser.pause(2000)
+      await $('a[href="https://staging-new.a2odev.com/"]').moveTo({})
+      await browser.pause(2000)
+    })  
+})
