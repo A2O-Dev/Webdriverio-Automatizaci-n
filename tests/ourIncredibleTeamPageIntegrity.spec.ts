957257389174dad96b82a987479b149a55cdac1d
Karina Cabrera <kart.p14@gmail.com>
2023-03-09 10:44:33 -0400

    RI-856 Adding contactUs verification and social network icons.

--- tests/ourIncredibleTeamPageIntegrity.spec.ts
+++ tests/ourIncredibleTeamPageIntegrity.spec.ts
@@ -4,11 +4,10 @@ describe('Check the integrity of the UI/UX of the Our Incredible Team', () => {
         await browser.pause(3000)
         await browser.maximizeWindow()
         await browser.pause(3000)
-        // Variables Our Incredible Team section
+        // Variables 
         const titleSectionOurTeam = await $('.elementor-element-70c897fd .elementor-widget-container .elementor-heading-title')
         const paragraphOurTeam = await $('.elementor-element-96d2f7b .elementor-widget-container')
         const navHomeLink = await $('.elementor-element-7571f2d9 .elementor-widget-container').$('p').$('a')
-        // Variables of the team members section
         const titleSectionMembers = await $('.elementor-element-2499d793 .elementor-widget-container .elementor-heading-title')
         const paragraphMembers = await $('.elementor-element-3aa98e8b .elementor-widget-container')
         const nameMember1 = await $('.elementor-element-ee2314a .elementor-widget-container').$('p').$('strong')
@@ -19,43 +18,59 @@ describe('Check the integrity of the UI/UX of the Our Incredible Team', () => {
         const roleMember3 = await $('//section/div/div[3]/div/section/div/div/div/div[2]/div/p[2]')
         const nameMember4 = await $('.elementor-element-202364c .elementor-widget-container').$('p').$('strong')
         const roleMember4 = await $('//section/div/div[4]/div/section/div/div/div/div[2]/div/p[2]')
+        const facebookLink = await $('a[href="https://es-la.facebook.com/a2odev/"]')
+        const twitterLink = await $('a[href="https://twitter.com/a2odev"]')
+        const linkedinLink = await $('a[href="https://bo.linkedin.com/company/a2odev"]')
+        const homePage = await $('.elementor-element-5c525402 .elementor-widget-container .elementor-icon-wrapper').$('a')
 
-        // Variables of 
-
-        await browser.scroll(500,500)
+        await browser.scroll(500, 500)
         await browser.pause(3000)
-
         await expect(titleSectionOurTeam).toHaveText('Our Incredible Team')
         await expect(paragraphOurTeam).toHaveText('The A2ODev team for web and mobile development is now growing fast. Simmilarly the number of solutions we provide in the fields of mobile apps and Software as a Service (SaaS) for Business and providers around the world.')
         await expect(navHomeLink).toHaveHref('https://staging-new.a2odev.com/')
-        //console.log(await (navHomeLink).getText())
-        //console.log(await (navHomeLink).getAttribute('href'))
         await expect(titleSectionMembers).toHaveText('Team Members')
         await expect(paragraphMembers).toHaveText('In A2odev we are always working together and collaborating. As a result we can provide high quality software solutions around the globe. Check out a little bit about our team for app and web development.')
-        
-        // Our web and application development team.
-       
-        await expect (nameMember1).toHaveText('Ariel Ayaviri')
-        await expect (roleMember1).toHaveText('CEO-CTO')
-        await expect (nameMember2).toHaveText('César Tablero')
-        await expect (roleMember2).toHaveText('Server Admin – Full Stack Developer')
-        await expect (nameMember3).toHaveText('Antonio Arano')
-        await expect (roleMember3).toHaveText('Front End Developer')
-        await expect (nameMember4).toHaveText('Martin Guadama')
-        await expect (roleMember4).toHaveText('Full Stack Developer')
-        //console.log( nameMember1.getText()+'.........')
-        //console.log( roleMember1.getText())
 
-        // Checking hover
+        // Our web and application development team.
+        await expect(nameMember1).toHaveText('Ariel Ayaviri')
+        await expect(roleMember1).toHaveText('CEO-CTO')
+        await expect(nameMember2).toHaveText('César Tablero')
+        await expect(roleMember2).toHaveText('Server Admin – Full Stack Developer')
+        await expect(nameMember3).toHaveText('Antonio Arano')
+        await expect(roleMember3).toHaveText('Front End Developer')
+        await expect(nameMember4).toHaveText('Martin Guadama')
+        await expect(roleMember4).toHaveText('Full Stack Developer')
+
         await $('.elementor-element-f2a7397').moveTo()
         await browser.pause(2000)
+        await expect(nameMember1).toHaveText('Ariel Ayaviri')
+        await expect(roleMember1).toHaveText('CEO-CTO')
         await $('.elementor-element-1512238').moveTo()
+        await expect(nameMember2).toHaveText('César Tablero')
+        await expect(roleMember2).toHaveText('Server Admin – Full Stack Developer')
         await browser.pause(2000)
         await $('.elementor-element-4738aa4').moveTo()
         await browser.pause(2000)
+        await expect(nameMember3).toHaveText('Antonio Arano')
+        await expect(roleMember3).toHaveText('Front End Developer')
         await $('.elementor-element-9e6e5a4').moveTo()
         await browser.pause(2000)
-        await expect (await $('.elementor-element-2d25c6f9  .elementor-widget-container .elementor-heading-title')).toHaveText('A2ODEV is Awesome !')
+        await expect(nameMember4).toHaveText('Martin Guadama')
+        await expect(roleMember4).toHaveText('Full Stack Developer')
+        await expect(await $('.elementor-element-2d25c6f9  .elementor-widget-container .elementor-heading-title')).toHaveText('A2ODEV is Awesome !')
+
+        // Contact Us
+        await $('.elementor-element-4f9cf0d8').click()
         await browser.pause(2000)
+        await expect($('.elementor-element-8f78e74 .elementor-widget-container')).toHaveText('Contact Us')
+        await expect($('#name')).toBeExisting()
+        await expect($('#email')).toBeExisting()
+        
+        await $('.elementor-element-499d5edd').scrollIntoView({ block: 'center', inline: 'center' });
+        await browser.pause(2000)
+        await expect(facebookLink).toBeExisting()
+        await expect(twitterLink).toBeExisting()
+        await expect(linkedinLink).toBeExisting()
+        await expect(homePage).toHaveHref('https://staging-new.a2odev.com/')
     })
 })
