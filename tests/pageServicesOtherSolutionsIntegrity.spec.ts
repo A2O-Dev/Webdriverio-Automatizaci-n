cb5071e55b88bbf0312476c69fcceb601fb940ac
Julia Cabrera <julia.cabrera@a2odev.com>
2023-03-30 11:44:53 -0400

    feature/RI-912 Updating code

--- tests/pageServicesOtherSolutionsIntegrity.spec.ts
+++ tests/pageServicesOtherSolutionsIntegrity.spec.ts
@@ -1,47 +1,23 @@
 describe('Check the integrity of the UI of the Other Solutions page in Services', () => {
     it('Page of Other Solutions', async () => {
-        await browser.url('https://staging-new.a2odev.com/')
-        await browser.pause(3000)
+        await browser.url('https://staging-new.a2odev.com/other-solutions/')
         await browser.maximizeWindow()
-        await browser.pause(2000)
 
-        const otherSolutionsLink = await $('.elementor-element-1a769d2')
-        const aboutUsLink = await $('#premium-nav-menu-item-1897 a')
-        const servicesLink = await $('#premium-nav-menu-item-14432 a')
-        const projectsLink = await $('#premium-nav-menu-item-1899 a')
-        const faqLink = await $('#premium-nav-menu-item-1900 a')
-        const contactUsLink = await $('#premium-nav-menu-item-14425 a')
-        const iconLenguage = await $('//*[@id="premium-nav-menu-item-8907"]/a/span/img')
-
-        // Services section
-        await servicesLink.click()
-        await browser.pause(3000)
-
-        // Sections of the page Services - Other Solutions
-
-        await otherSolutionsLink.click()
-        await browser.pause(3000)
-
-        // Main navigation menu
-        await expect(aboutUsLink).toBeExisting()
-        await expect(aboutUsLink).toHaveHref('https://staging-new.a2odev.com/about-us/')
-        await expect(servicesLink).toBeExisting()
-        await expect(servicesLink).toHaveHref('https://staging-new.a2odev.com/#services')
-        await expect(projectsLink).toBeExisting()
-        await expect(projectsLink).toHaveHref('https://staging-new.a2odev.com/projects/')
-        await expect(faqLink).toBeExisting()
-        await expect(faqLink).toHaveHref('https://staging-new.a2odev.com/faq/')
-        await expect(contactUsLink).toHaveHref('#contact-us')
-        await expect(iconLenguage).toHaveAttribute('src', 'https://staging-new.a2odev.com/wp-content/plugins/translatepress-multilingual/assets/images/flags/en_US.png')
-
+        // Variables
+        const aboutUsLink = await $('=About us')
+        const servicesLink = await $('=Services')
+        const projectsLink = await $('=Projects')
+        const faqLink = await $('=FAQ')
+        const contactUsLink = await $('=Contact us')
+        const iconLenguage = await $('img[title="English"]')
         const firstTitle = await $('h2=Other Solutions')
-        const paragraph = await $('.elementor-element-316879d2 .elementor-widget-container')
-        const item1 = await $('//ul[@class="elementor-icon-list-items"]/li[1]')
-        const item2 = await $('//ul[@class="elementor-icon-list-items"]/li[2]')
-        const item3 = await $('//ul[@class="elementor-icon-list-items"]/li[3]')
-        const image = await $('.elementor-element-754fbdf5 .elementor-widget-container img')
-        const scrollContact = await $('.elementor-element-1dc5bebe')
-        const titleContactUs = await $('.elementor-element-6ade596f .elementor-widget-container h3')
+        const paragraph = await $('p=Other services we excel in:')
+        const item1 = await $('.elementor-icon-list-items li:nth-child(1)')
+        const item2 = await $('.elementor-icon-list-items li:nth-child(2)')
+        const item3 = await $('.elementor-icon-list-items li:nth-child(3)')
+        const image = await $('img[src="https://staging-new.a2odev.com/wp-content/uploads/2023/01/Recurso-7@3x-1-1024x651.png"]')
+        const scrollContact = await $('.contact-footer')
+        const titleContactUs = await $('h3=Contact Us')
         const inputName = await $('#name')
         const inputEmail = await $('#email')
         const inputWebsite = await $('[name="website"]')
@@ -50,19 +26,27 @@ describe('Check the integrity of the UI of the Other Solutions page in Services'
         const facebookLink = await $('a[href="https://es-la.facebook.com/a2odev/"]')
         const twitterLink = await $('a[href="https://twitter.com/a2odev"]')
         const linkedinLink = await $('a[href="https://bo.linkedin.com/company/a2odev"]')
-        const homePage = await $('.elementor-element-6e0eed79 .elementor-widget-container .elementor-icon-wrapper a')
+        const homePage = await $('.inicio .elementor-widget-container .elementor-icon-wrapper a')
+        const buttonGoToTop = await $('a[href="#scroll-up"]')
+
+        // Main navigation menu
+        await expect(aboutUsLink).toHaveHref('https://staging-new.a2odev.com/about-us/')
+        await expect(servicesLink).toHaveHref('https://staging-new.a2odev.com/#services')
+        await expect(projectsLink).toHaveHref('https://staging-new.a2odev.com/projects/')
+        await expect(faqLink).toHaveHref('https://staging-new.a2odev.com/faq/')
+        await expect(contactUsLink).toHaveHref('#contact-us')
+        await expect(iconLenguage).toHaveAttribute('src', 'https://staging-new.a2odev.com/wp-content/plugins/translatepress-multilingual/assets/images/flags/en_US.png')
 
         await expect(firstTitle).toBeExisting()
         await expect(image).toBeExisting()
-        await expect(paragraph).toHaveTextContaining('Other services we excel in:')
+        await expect(paragraph).toBeExisting()
         await expect(item1).toHaveTextContaining('REST API development.')
         await expect(item2).toHaveTextContaining('Server Maintenance.')
         await expect(item3).toHaveTextContaining('Image Processing.')
 
         // Contact Us
         await scrollContact.scrollIntoView(false)
-        await browser.pause(3000)
-        await expect(titleContactUs).toHaveTextContaining('Contact Us')
+        await expect(titleContactUs).toBeExisting()
         await expect(inputName).toBeExisting()
         await expect(inputEmail).toBeExisting()
         await expect(inputWebsite).toBeExisting()
@@ -71,19 +55,15 @@ describe('Check the integrity of the UI of the Other Solutions page in Services'
 
         // Social Networking
         await scrollContact.scrollIntoView({ block: 'center', inline: 'center' })
-        await browser.pause(3000)
         await expect(facebookLink).toBeExisting()
         await expect(twitterLink).toBeExisting()
         await expect(linkedinLink).toBeExisting()
         await expect(homePage).toHaveHref('https://staging-new.a2odev.com/')
 
         // Floating button to go to the top of the page.
-
-        const buttonGoToTop = await $('a[href="#scroll-up"]')
-
-        await expect(buttonGoToTop).toBeExisting()
+        await expect(buttonGoToTop).toHaveAttribute('class', 'elementor-icon active')
         await buttonGoToTop.click()
-        await browser.pause(3000)
+        await expect(buttonGoToTop).not.toHaveAttribute('class', 'active')
 
     })
 })
