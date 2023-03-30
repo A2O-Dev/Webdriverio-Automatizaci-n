229d65cd2f6eb10d5fdc94225b399be40eeb41e5
Julia Cabrera <julia.cabrera@a2odev.com>
2023-03-30 12:05:33 -0400

    feature/RI-891 update

--- tests/pageServiceWebAppDevIntegrity.spec.ts
+++ tests/pageServiceWebAppDevIntegrity.spec.ts
@@ -34,7 +34,7 @@ describe('Check the integrity of the UI of the Web App Development page in Servi
         await expect(projectsLink).toHaveHref('https://staging-new.a2odev.com/projects/')
         await expect(faqLink).toHaveHref('https://staging-new.a2odev.com/faq/')
         await expect(contactUsLink).toHaveHref('#contact-us')
-        await expect(iconLenguage).toBeExisting()
+        await expect(iconLenguage).toHaveAttribute('src', 'https://staging-new.a2odev.com/wp-content/plugins/translatepress-multilingual/assets/images/flags/en_US.png')
 
         // Sections of the page Services - Web Apps Development
         await expect(firstTitle).toBeExisting()
