875c0eade63df8b5f1a3a70b21b6b0497a4e6acd
Giselle026 <giselle.castro@a2odev.com>
2023-04-26 16:57:28 -0400

    feature/RI-934

--- tests/aboutUs/aboutUsIntegrity.spec.ts
+++ tests/aboutUs/aboutUsIntegrity.spec.ts
@@ -1,31 +1,37 @@
-import { navigationMenu }  from '../../dictionaries/selectors/index.ts'
+import { aboutUs, contactUs, navigationMenu }  from '../../dictionaries/selectors/index.ts'
+import * as dotenv from 'dotenv'
+
+const envFound = dotenv.config();
+if (envFound.error) {
+  throw new Error("Couldn't find .env file");
+}
+const env = process.env
 describe('About Us Integrity', () => {
-    it('About us Integrity laptop', async () => {
-      await browser.url('https://staging-new.a2odev.com/')
-      await browser.maximizeWindow()
-      
-      const aboutUsLink = await $(navigationMenu.aboutUsLink)
-      const contactUsLink =await $(navigationMenu.contactUsLink)
+  it('About us Integrity laptop', async () => {
+    await browser.url(env.A2O_DEV_URL)
+    await browser.maximizeWindow()
 
-      aboutUsLink.click()
-      await browser.pause(3000)
-      await expect($('h1=About us')).toBeExisting()
-      await expect($('//*[@id="scroll-up"]/div/div[2]/div/div[2]/div/p')).toHaveTextContaining('We are a team of passionate programmers with high expertise in development of mobile and web apps.')
-      await expect($('//*[@id="scroll-up"]/div/div[2]/div/div[3]/div/ul/li[1]/span[2]')).toHaveTextContaining('Responsability')
-      await expect($('//*[@id="scroll-up"]/div/div[2]/div/div[3]/div/ul/li[2]/span[2]')).toHaveTextContaining('Fluent Communication')
-      await expect($('//*[@id="scroll-up"]/div/div[2]/div/div[3]/div/ul/li[3]/span[2]')).toHaveTextContaining('Adaptability')
-      await expect($('//*[@id="scroll-up"]/div/div[2]/div/div[3]/div/ul/li[4]/span[2]')).toHaveTextContaining('Rockstar Developers')
-      await expect($('//*[@id="scroll-up"]/div/div[2]/div/div[3]/div/ul/li[5]/span[2]')).toHaveTextContaining('Love Challenges')
-      await expect($('//*[@id="scroll-up"]/div/div[2]/div/div[3]/div/ul/li[6]/span[2]')).toHaveTextContaining('Constantly Updated')
-      await expect($('//*[@id="scroll-up"]/div/div[2]/div/div[3]/div/ul/li[7]/span[2]')).toHaveTextContaining('Use of best practices')
-      await expect($('//*[@id="scroll-up"]/div/div[1]/div/div/div/img')).toBeExisting()
-      await browser.pause(3000)
+    const aboutUsLink = await $(navigationMenu.aboutUsLink)   
+    const contanctUsLink = await $(navigationMenu.aboutUsLink) 
 
-      contactUsLink.click()
-      await expect($('[id="contact-us"]')).toBeExisting()
-      await browser.pause(3000)
-    })
+    await aboutUsLink.click()
+    await expect($(aboutUs.titleAboutUs)).toBeExisting()
+    await expect($(aboutUs.paragraphAboutUs)).toHaveTextContaining('We are a team of passionate programmers with high expertise in development of mobile and web apps.')
+    await expect($(aboutUs.listResponsability)).toHaveTextContaining('Responsability')
+    await expect($(aboutUs.listFluent)).toHaveTextContaining('Fluent Communication')
+    await expect($(aboutUs.listAdaptability)).toHaveTextContaining('Adaptability')
+    await expect($(aboutUs.listRockstar)).toHaveTextContaining('Rockstar Developers')
+    await expect($(aboutUs.listLove)).toHaveTextContaining('Love Challenges')
+    await expect($(aboutUs.listConstantly)).toHaveTextContaining('Constantly Updated')
+    await expect($(aboutUs.listUse)).toHaveTextContaining('Use of best practices')
+    await expect($(aboutUs.mainImg)).toBeExisting()
+    
+    await contanctUsLink.click()
+    await expect($(contactUs.contactUsForm)).toBeExisting()
+
+  })
 })
+
 
 
 
