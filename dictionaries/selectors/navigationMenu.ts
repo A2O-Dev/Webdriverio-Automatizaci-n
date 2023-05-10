af175052f4c85bdd11577bb5d7506d1eca608f4d
GiselleA2ODEV <giselle.castro@a2odev.com>
2023-05-09 23:38:19 -0400

    feature/RI-933

--- dictionaries/selectors/navigationMenu.ts
+++ dictionaries/selectors/navigationMenu.ts
@@ -1,4 +1,6 @@
 export const navigationMenu = {
+  CoverTitle             :'h1=We turn your ideas into High Quality products !',
+  coverSubtitle          :'//p[contains(text(),"Evolve your idea to an MVP and more. Impress your ")]',  
   imgMoviesCasting       :'.attachment-full.size-full.wp-image-8912',
   logoTypeImg            :'.attachment-full.size-full.wp-image-8037',
   aboutUsLink            :'=About us',
@@ -9,4 +11,7 @@ export const navigationMenu = {
   languageLink           :'//*[@id="premium-nav-menu-item-8907"]',
   subMenuLink            :'//*[@id="premium-nav-menu-item-8907"]/ul',
   spanishFlag            :'img[title="Spanish"]',
+  smartCard              :'h3=Smart Solutions',
+  interactiveCard        :'h3=Interactive Development',
+  readyCard              :'h3=Ready to Scale',
 }
--- tests/homePage/homePageIntegrity.spec.ts
+++ tests/homePage/homePageIntegrity.spec.ts
@@ -11,20 +11,23 @@ describe('Home Page Integrity', () => {
         await browser.url(env.A2O_DEV_URL)
         await browser.maximizeWindow()
 
-        const logoTypeImg = await $(navigationMenu.logoTypeImg)
-        const imgMoviesCasting = await $(navigationMenu.imgMoviesCasting)
         const aboutUsLink = await $(navigationMenu.aboutUsLink)
         const servicesLink = await $(navigationMenu.servicesLink)
         const projectsLink = await $(navigationMenu.projectsLink)
         const faqLink = await $(navigationMenu.faqLink)
         const contactUsLink = await $(navigationMenu.contactUsLink)
         const languageLink = await $(navigationMenu.languageLink)
+
+        const smartCard = await $(navigationMenu.smartCard)
+        const interactiveCard = await $(navigationMenu.interactiveCard)
+        const readyCard = await $(navigationMenu.readyCard)
+
         const goToTopButton = await $(footer.goToTopButton)
 
-        await expect($(logoTypeImg)).toBeExisting()
-        await expect($(imgMoviesCasting)).toBeExisting()
-        await expect($('h1=We turn your ideas into High Quality products !')).toBeExisting()
-        await expect($('//*[@id="scroll-up"]/div/div[1]/div/div[2]/div/p')).toBeExisting()
+        await expect($(navigationMenu.logoTypeImg)).toBeExisting()
+        await expect($(navigationMenu.imgMoviesCasting)).toBeExisting()
+        await expect($(navigationMenu.CoverTitle)).toBeExisting()
+        await expect($(navigationMenu.coverSubtitle)).toBeExisting()
 
         aboutUsLink.moveTo({})
         servicesLink.moveTo({})
@@ -39,6 +42,21 @@ describe('Home Page Integrity', () => {
         await expect($(faqLink)).toBeExisting()
         await expect($(contactUsLink)).toBeExisting()
         await expect($(languageLink)).toBeExisting()
+        await browser.scroll(0, 700)
+      
+
+        smartCard.moveTo({})
+        interactiveCard.moveTo({})
+        readyCard.moveTo({})
+        
+        await expect($(smartCard)).toBeExisting()
+        await expect($(interactiveCard)).toBeExisting()
+        await expect($(readyCard)).toBeExisting()
+        
+        await goToTopButton.click()
         await expect($(goToTopButton)).toBeExisting()
+
+
+
     })
 })
