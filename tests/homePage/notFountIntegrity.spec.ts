b99bf3f9ec88d5be1d62c2e71ac004ad69ccd13f
GiselleA2ODEV <giselle.castro@a2odev.com>
2023-06-08 23:42:59 -0400

    feature/RI-854

---
+++ tests/homePage/notFountIntegrity.spec.ts
@@ -0,0 +1,23 @@
+import { fount } from '../../dictionaries/selectors/index.ts'
+import { urls } from '../../dictionaries/config/index.ts'
+
+describe('Check Not Found Integrity', () => {
+    it('Check Url: Not Found', async () => {
+        await browser.url(urls.notFount)
+        await browser.maximizeWindow()
+
+
+        const numberFour1 = await $(fount.numberFour1)
+        const numberZero = await $(fount.numberZero)
+        const numberFour2 = await $(fount.numberFour2)
+        const linkHomePage = await $(fount.linkHomePage)
+
+        await expect(numberFour1).toHaveTextContaining('4')
+        await expect(numberZero).toHaveTextContaining('0')
+        await expect(numberFour2).toHaveTextContaining('4')
+        await expect(linkHomePage).toHaveTextContaining('Take me to the Home Page')
+        await linkHomePage.click()
+        await browser.url(urls.host)
+
+    })
+})
